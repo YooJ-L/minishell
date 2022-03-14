ccd52b8493be1d44e5c1beb8a79b6a45f7fbd634
이유진 <yoojlee42@gmail.com>
2022-03-14 20:09:36 +0900

    NEED: unset.c/ validate_unset_arg

--- srcs/built_in_functions/export.c
+++ srcs/built_in_functions/export.c
@@ -6,7 +6,7 @@
 /*   By: yoojlee <yoojlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 12:01:27 by yoojlee           #+#    #+#             */
-/*   Updated: 2022/03/14 18:58:19 by yoojlee          ###   ########.fr       */
+/*   Updated: 2022/03/14 19:34:00 by yoojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,8 @@
 1. export a122 => a122=
 2. export 1a => export: not an identifier: 1a
  */
+
+int	execute_export(t_info *info, t_process *process)
+{
+	return (1);
+}
--- srcs/built_in_functions/unset.c
+++ srcs/built_in_functions/unset.c
@@ -6,7 +6,7 @@
 /*   By: yoojlee <yoojlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 21:27:30 by yoojlee           #+#    #+#             */
-/*   Updated: 2022/03/14 19:08:23 by yoojlee          ###   ########.fr       */
+/*   Updated: 2022/03/14 20:08:12 by yoojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,38 +20,68 @@
 4. unset 숫자 x ex) unset 12a(x)
 5. unset 문자+숫자 o ex) unset a999, unset abcd123
 6. unset @ 들어가면 X 앞뒤에 뭐가 오든 안됨.
-7. unset 1a(안되는 것) a(지울 것) b(지울 것) => a b 지워지고 bash: unset: `1a': not a valid identifier 나옴.
+7. unset 1a(잘못된 인자) a(되는 것) b(되는 것) => a b 지워지고 bash: unset: `1a': not a valid identifier 나옴. 
+	안되는 것 여러개 일 때 => 다 출력 	bash: unset: `1a': not a valid identifier
+								bash: unset: `2c': not a valid identifier
+8. unset (...) => bash: syntax error near unexpected token `...'
+9. echo "없는 것" => exit status=0(정상 작동 취급)
+	echo "인자가 잘못 된 것 (1a, 11 같은거)" => exit status=1
+	echo "잘못된 인자" "없는 것" => exit status=1 (인자 순서 바꿔도 1) (7번 에러문구는 출력됨)
+	==> 잘못된 인자가 있으면 에러코드는 1! 다른 건 0!
 */
 
-void	delete_content(t_env *env, char *key)
+int	delete_head_content(t_env **env, char *key)
 {
-	t_env	*node_to_link;
+	t_env	**head;
 
-	while (env->next)
+	head = env;
+	if (ft_strncmp((*head)->key, key, ft_strlen(key) + 1) == 0)
 	{
-		if (ft_strncmp(env->key, key, ft_strlen(key) + 1) == 0)
+		env = &((*env)->next);
+		free((*head)->key);
+		free((*head)->value);
+		free(*head);
+		return (1);
+	}
+	return (0);
+}
+
+void	delete_content(t_env **env, char *key)
+{
+	t_env	*this_node;
+	t_env	*previous_node;
+
+	if (delete_head_content(env, key))
+		return ;
+	this_node = *env;
+	while (this_node->next)
+	{
+		if (ft_strncmp(this_node->key, key, ft_strlen(key) + 1) == 0)
 		{
-			free(env->key);
-			free(env->value);
-			node_to_link->next = env->next;
-			env->next = NULL;
-			free(env);
+			free(this_node->key);
+			free(this_node->value);
+			previous_node->next = this_node->next;
+			this_node->next = NULL;
+			free(this_node);
+			return ;
 		}
-		node_to_link = env;
-		env = env->next;
+		previous_node = this_node;
+		this_node = this_node->next;
 	}
 }
 
-bool	validate_unset_arg(char *content)
+bool	validate_unset_arg(char *content) //잘못된 인자인지 체크
 {
+	if (ft_isalnum(content[0])) //첫 글자가 숫자이면 error
+		return (false);
 	return (true);
 }
 
-int		execute_unset(t_info *info, t_process *process)
+int	execute_unset(t_info *info, t_process *process)
 {
-	bool	success_flag;
+	bool	wrong_arg;
 
-	success_flag = 1;
+	wrong_arg = 0;
 	if (!process->arg)
 	{
 		//unset: not enough arguments
@@ -60,16 +90,21 @@ int		execute_unset(t_info *info, t_process *process)
 	{
 
 	}
-	while (process->arg->next)
+	while (process->arg->next) //인자 여러개여도 가능하니까 arg리스트 반복문 돌림.
 	{
-		if (!validate_unset_arg(process->arg->content))
+		if (!validate_unset_arg(process->arg->content)) //잘못된 인자 들어왔는지 체크
 		{
-			success_flag = 0;
+			wrong_arg = 1;
+			printf("bash: unset: `%s': not a valid identifier", process->arg->content);
 			process->arg = process->arg->next;
 			continue ;
 		}
-		delete_content(info->env, process->arg->content);
+		delete_content(&info->env, process->arg->content);
 		process->arg = process->arg->next;
+	}
+	if (wrong_arg)
+	{
+		//exit_status == 1
 	}
 	return (1);
 }
