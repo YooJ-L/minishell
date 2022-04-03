ccc1447707ad532d76904369758346907af00515
Dasom Im <dim@c8r8s10.42seoul.kr>
2022-04-03 21:46:50 +0900

    ING: check_token 0403

--- includes/parsing.h
+++ includes/parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dim <dim@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: dim <dim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 03:46:19 by dim               #+#    #+#             */
-/*   Updated: 2022/04/02 03:48:49 by dim              ###   ########.fr       */
+/*   Updated: 2022/04/03 20:10:04 by dim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 # define PARSING_H
 
 # include "structure.h"
-#define NO_Q 0
-#define SINGLE_Q 1
-#define DOUBLE_Q 2
+# define NO_Q 0
+# define SINGLE_Q 1
+# define DOUBLE_Q 2
+
+# define INSTRUCTION 1
+# define OPTION 2
+# define ARG 3
+# define REDIRECTION 4
+# define FILENAME 5
+
 
 int		check_quote(const char *chr, int *status);
 
--- srcs/parse/parse_process.c
+++ srcs/parse/parse_process.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_process.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dim <dim@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: dim <dim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 02:55:31 by dim               #+#    #+#             */
-/*   Updated: 2022/04/03 16:14:51 by dim              ###   ########.fr       */
+/*   Updated: 2022/04/03 21:23:03 by dim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,7 +140,8 @@ char	*redirection_token(const char *str)
 
 	cnt = 0;
 	while (str[0] == str[cnt] && cnt < 2)
-	// >일때는..?
+		cnt++;
+	// >일때는..? cnt가 0이라 빈문자열만 저장..?
 	ret = (char *)malloc(sizeof(char) * (cnt + 1));
 	if (ret == NULL)
 		perror_and_exit("cannot allocate memory\n", ENOMEM);
@@ -150,30 +151,36 @@ char	*redirection_token(const char *str)
 
 char	*other_token(const char *str)
 {
-	int		i;
+	// 결론은 space 단위로 토큰을 내는것
+	int		cnt;
 	int		status;
 	char	*ret;
 
-	i = 0;
+	cnt = 0;
 	stauts = 0;
-	while ((str[i] != ' ' && str[i] != '\0' \
-			&& str[i] != '<' && str[i] != '>') \
-			|| (status != NO_Q && str[i] != '\0'))
+	// space나 빈문자열, 리다이렉션이 아닌 문자이거나 quote 안에 있는 빈문자열이 아닌 문자들을 체크하고 카운트함
+	while ((str[cnt] != ' ' && str[cnt] != '\0' \
+			&& str[cnt] != '<' && str[cnt] != '>') \
+			|| (status != NO_Q && str[cnt] != '\0'))
 	{
-		check_quote(str[i], &status);
-		i++;
+		check_quote(str[cnt], &status);
+		cnt++;
 	}
+	// quote가 짝이 안맞았는데 빈문자열을 만나는 조건에 걸림
 	if (status != NO_Q)
 	{
-		i = 1;
-		while (str[i] != ' ' && str[i] != '\0')
-			i++;
+		// cnt는 1로 초기화 후에 다시 1번 문자열부터 검사
+		// ?????
+		cnt = 1;
+		// space이거나 빈문자열이 올때까지 카운트
+		while (str[cnt] != ' ' && str[cnt] != '\0')
+			cnt++;
 	}
-	ret = (char *)malloc(sizeof(char) * (i + 1));
+	ret = (char *)malloc(sizeof(char) * (cnt + 1));
 	if (ret == NULL)
 		perror_and_exit("cannot allocate memory\n", ENOMEM);
-	ft_strlcpy(ret, str, i + 1);
-	retur (ret);
+	ft_strlcpy(ret, str, cnt + 1);
+	return (ret);
 }
 
 char	*get_one_token(const char *str)
@@ -182,6 +189,71 @@ char	*get_one_token(const char *str)
 		return (redirection_token(str));
 	else
 		return (other_token(str));
+}
+
+int		classify_token(t_process *process, const char *token, int tag)
+{
+	int		i;
+
+	if (token[0] == '<' || token[0] == '>')
+	{
+		if (tag == REDIRECTION)
+		{
+			perror_in_parsing((char *)token);
+			return (258);
+		}
+		else
+			return (REDIRECTION);
+	}
+	if (tag == REDIRECTION)
+		return (FILENAME);
+	if (process->instruction == NULL)
+		return (INSTRUCTION);
+	if (token[0] == '-' && process->arg)
+	{
+		i = 1;
+		while (token[i] == '\"' || token[i] == '\'')
+			i++;
+		if (ft_isalpha(token[i]))
+			return (OPTION);
+	}
+	return (ARG);
+}
+
+void	remove_outside_quotes_in_str(char **str)
+{
+	int		cnt;
+	int		status;
+	char	*new_str;
+
+	cnt = 0;
+	status = NO_Q;
+	new_str = NULL;
+	while ((*str)[i])
+	{
+		if (check_quote((*str)[cnt], &status))
+		{
+			cnt++;
+			continue ;
+		}
+		add_char_to_str(&new_str, (*str)[cnt]);
+		cnt++;
+	}
+	free(*str);
+	*str = new_str;
+}
+
+int		check_token(t_porcess *process, char **new_str, char **cur_token, int *tag)
+{
+	*tag = classify_token(process, *cur_token, *tag);
+	if (*tag == 258)
+	{
+		free(*cur_token);
+		free(*new_str);
+		return (258);
+	}
+	remove_outside_quotes_in_str(cur_token);
+	return (0);
 }
 
 int		parse_process(t_process *process, t_info *info, const char *line, int len)
@@ -200,7 +272,8 @@ int		parse_process(t_process *process, t_info *info, const char *line, int len)
 	{
 		cur_token = get_one_token(&new_line[i]);
 		i += ft_strlen(cur_token);
-		//check_token();
+		if (check_token(process, &new_line, &cur_token, &tag));
+			return (258);
 		//save_token();
 	}
 	free(new_str);
